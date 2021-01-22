64ba0b7f19f57f911bfacab83a209e43d22da02f
AlexMartin23 <832007@mystma.org>
2021-01-22 16:12:33 +0000

    factorial or sum between try 1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+def factOrSum(x, o):
+    if o == "factorial":
+        number = 1
+        for i in range (1, x + 1):
+            number = number * i
+        return number
+    else:
+        number = 0
+        for i in range (1, x):
+            number = number + i
+        return number
+
+print(factOrSum(4, "factorial"))
+print(factOrSum(10, "sum"))
